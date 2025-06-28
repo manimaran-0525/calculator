bfb93c82b5e2a81230a978e829e5391f9653f7f3
manimaran-0525 <Manimaran00144@gmail.com>
2025-06-28 20:37:19 +0530

    Calculator
    Using HTML,CSS,Java Script

---
+++ cal.js
@@ -0,0 +1,77 @@
+function add(){
+var f=document.getElementById("first").value
+var s=document.getElementById("second").value
+var a=Number(f)+Number(s)
+var a=document.getElementById("result").innerHTML=`<b>${a}</b>`}
+
+
+function sub(){
+    var f=document.getElementById("first").value
+var s=document.getElementById("second").value
+var a=Number(f)-Number(s)
+var a=document.getElementById("result").innerHTML=`<b>${a}</b>`}
+
+    
+    
+    function mul(){
+        var f=document.getElementById("first").value
+        var s=document.getElementById("second").value
+        var a=Number(f)*Number(s)
+        var a=document.getElementById("result").innerHTML=`<b>${a}</b>`}
+        
+        
+        function div(){
+            var f=document.getElementById("first").value
+            var s=document.getElementById("second").value
+            var a=Number(f)/Number(s)
+            var a=document.getElementById("result").innerHTML=`<b>${a}</b>`}
+            
+function cleared(){
+    var f= document.getElementById("first").value=""
+    document.getElementById("first").value=""
+var s=document.getElementById("second").value
+document.getElementById("second").value=""
+var a=Number(f)+Number(s)
+var a= document.getElementById("result").innerHTML=`<b>${a}</b>`.value=""
+}            
+
+
+// function add(){
+
+//     var card=document.createElement("div")
+//     var a=document.getElementById("A").value
+        
+
+// }
+
+
+
+// function add(){
+//  var i=document.getElementById("a").value
+//  var j=document.getElementById("result")
+//  var k=document.getElementById("parent")
+// var l=document.createElement("p")
+// l.innerHTML=`<h3>${i}</h3>`
+// k.appendChild(l)
+// }
+
+
+// function add(){
+//     var a=document.getElementById("m").value;
+
+//  var b=document.getElementById("result")
+//  var c=document.getElementById("parent")
+//  var d=document.createElement("p")
+//  d.innerHTML=`<h3>${a}</h3>`
+// c.appendChild(d)
+//  }
+
+
+
+
+
+
+
+
+
+
